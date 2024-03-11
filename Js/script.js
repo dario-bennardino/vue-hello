c36d57d5048cd8b6ffcc1e73464b62065cc4be49
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-03-11 16:40:58 +0100

    [add] bonus immagine

--- Js/script.js
+++ Js/script.js
@@ -10,7 +10,9 @@ createApp({
             nome : 'Mario',
             cognome : 'Rossi',
             nomeUtente : 'Inserisci nome e cognome',
-            miaClasseCss : 'verde'
+            miaClasseCss : 'verde',
+            newImage : 'https://www.laramind.com/blog/wp-content/uploads/2019/05/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg',
+            sizeImage : 'fix-img',
 
         }
     }
